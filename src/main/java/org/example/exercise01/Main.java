d0c2e710ac85ed572a22d5fbe2d07b547fa716eb
Catalin Barsan <bcn@catalinbarsan.es>
2024-10-15 13:16:24 +0200

    Remove "Return" of main.

--- src/main/java/org/example/exercise01/Main.java
+++ src/main/java/org/example/exercise01/Main.java
@@ -1,12 +1,12 @@
 package org.example.exercise01;
 
-import java.net.URISyntaxException;
 
 public class Main {
+
     public static void main(String[] args) {
+
         if (args.length == 0) {
             System.out.println("Please provide a directory path as an argument.");
-            return;
         }
 
         String directoryPath = args[0];
@@ -19,5 +19,6 @@ public class Main {
         }
     }
 }
+
 
 
